8add54761c045b7dd5fef2c9a3800b77c7f3ded3
Sviatoslav Kovalev <svyatoslav.kovalev@yahoo.com>
2026-03-24 17:02:49 +0300

    in-between commit (WiP)

--- src/USB_8SMC5.py
+++ src/USB_8SMC5.py
@@ -1,6 +1,7 @@
 from serial import Serial
 import serial
 from serial.tools import list_ports
+import time
 
 class USB_8SMC5:
     def __init__(self):
@@ -16,8 +17,10 @@ class USB_8SMC5:
             if self.gser() is not None:
                 break
         if self.conn is None:
-            raise StandaMotorsNotFound()
-    
+            raise StandaMotorNotFound()
+
+
+
     def modbus_crc(self, msg:str) -> int:
         '''
         TODO use crcmod insetad!
@@ -32,7 +35,89 @@ class USB_8SMC5:
                 else:
                     crc >>= 1
         return crc
+
+
+
+    def gmov(self):
+        '''
+        Read movement settings (speed, acceleration, threshold, etc.).
+        '''
+        self.conn.write(str.encode('gmov'))
+        st = self.conn.read(30)
+
+        crc = self.modbus_crc(st[4:28])
+        ba = crc.to_bytes(2, byteorder='little')
+
+        crc_matches = ba[0] == st[28] and ba[1] == st[29]
+
+        if not crc_matches:
+            raise CrcNotMatches()
+        
+        print(f"speed: {int.from_bytes(st[4:7], byteorder='little')}")
+        print(f"uSpeed: {st[8]}")
+        print(f"accel: {int.from_bytes(st[9:10], byteorder='little')}")
+        print(f"decel: {int.from_bytes(st[11:12], byteorder='little')}")
+        print(f"antiplaySpeed: {int.from_bytes(st[13:16], byteorder='little')}")
+        print(f"uAntiplaySpeed: {st[17]}")
+        print(f"moveFlags: {st[18]}")
+
+
+        return st
     
+    
+
+    def geds(self):
+        '''
+        Read border and limit switches settings.
+        '''
+        pass
+    def gsec(self):
+        '''
+        Read settings of step motor power control. Used with a stepper motor only.
+        '''
+        pass
+    def geng(self):
+        '''
+        Read engine settings. This function reads the structure containing a set of useful motor settings stored
+        in the controller’s memory. These settings specify motor shaft movement algorithm, list of limitations
+        and rated characteristics.
+        '''
+        # TODO
+        pass
+    def gfbs(self):
+        pass
+    def gpwr(self):
+        pass
+    def gbrk(self):
+        pass
+    def gctl(self):
+        pass
+    def gjoy(self):
+        pass
+    def gctp(self):
+        pass
+    def ghom(self):
+        pass
+    def gpid(self):
+        pass
+    def gsni(self):
+        pass
+    def gsno(self):
+        pass
+    def gurt(self):
+        pass
+    def geio(self):
+        pass
+    def gemf(self):
+        pass
+    def gnmf(self):
+        pass
+    def gfwv(self):
+        pass
+    def geti(self):
+        pass
+
+
     def gets(self):
         self.conn.write(str.encode('gets'))
         st = self.conn.read(54)
@@ -74,6 +159,54 @@ class USB_8SMC5:
         #print("CRC16/MODBUS: %02X %02X"%(ba[0], ba[1]))
         #print(f"original crc = {hex(serial_num_raw[8]), hex(serial_num_raw[9])}")
     
+
+
+    def smov(self,
+             speed : int = 0,
+             uSpeed : int = 0,
+             accel : int = 0,
+             decel : int = 0,
+             antiplaySpeed : int = 0,
+             uAntiplaySpeed : int = 0,
+             moveFlags : int = 0):
+        
+        '''
+        uint32_t Speed          Target speed(for stepper motor: steps/s, for DC:rpm). Range:0..100000.
+        uint8_t  uSpeed         Target speed in micro step fractions/s.
+        uint16_t accel          Motor shaft acceleration, steps/s^2(step per motor) or RPM/s (DC). Range: 1..65535.
+        uint16_t decel          Motor shaft deceleration, steps/s^2(step per motor) or RPM/s (DC). Range: 1..65535.
+        uint32_t antiplaySpeed  Speed in antiplay mode, full steps/s (steppermotor) orRPM(DC). Range: 0..100000.
+        uint8_t  uAntiplaySpeed 
+        uint8_t  moveFlags      0x1-RPM_DIV_1000 --  Indicates that the operating speed specified in the command is set in milliRPM.
+                                Applicableonly for ENCODER feedbackmode and only for BLDCmotors.
+        '''
+
+        data =  bytearray(int(speed).to_bytes(4,  "little", signed=False)) # speed
+        data += bytearray(int(0).to_bytes(1, "little", signed=False)) # uSpeed
+        data += bytearray(int(0).to_bytes(2, "little", signed=False)) # accel
+        data += bytearray(int(0).to_bytes(2, "little", signed=False)) # decel
+        data += bytearray(int(0).to_bytes(4, "little", signed=False)) # antiplaySpeed
+        data += bytearray(int(0).to_bytes(1, "little", signed=False)) # uantiplaySpeed
+        data += bytearray(int(0).to_bytes(1, "little", signed=False)) # moveFlags
+        data += bytearray(int(0).to_bytes(9, "little", signed=False)) # reserved
+
+        # Calculate crc
+        crc = self.modbus_crc(data).to_bytes(4, "little")[0:2]
+
+        cmd = bytearray(str.encode("smov"))
+        cmd += data
+        cmd += crc
+
+        # Send packet and get answer
+        self.conn.write(cmd)
+        ret = self.conn.read(4)
+
+        # TODO Check answer for error!
+
+        return ret
+    
+
+
     def move(self, pos):
         # Fill data of frame
         data = bytearray(pos.to_bytes(4, "little", signed=True))
@@ -123,7 +256,21 @@ class USB_8SMC5:
         ret = self.conn.read(4)
         print(ret)
 
-class StandaMotorsNotFound(Exception):
+
+    def wait_for_stop(self, t = 0.1):
+        st = self.gets()
+        while st[5] & 0x80:
+            time.sleep(t)
+            st = self.gets()
+    
+    def wait_for_dest(self, pos : int, t = 0.1):
+        cur_pos = int.from_bytes(self.gets()[9:12], byteorder='little')
+        while cur_pos != pos:
+            print(f"cur_pos={cur_pos}")
+            time.sleep(t)
+            cur_pos = int.from_bytes(self.gets()[9:12], byteorder='little')
+
+class StandaMotorNotFound(Exception):
     """Raised when no one standa motors found."""
     pass
 
--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from serial.serialutil import SerialException
 
 from USB_8SMC5 import USB_8SMC5
 
-if __name__ == "__main__":
+def main():
     parser = argparse.ArgumentParser(
             prog='main.py',
             description='Program provides cli ineterface to run some commands...',
@@ -13,9 +13,12 @@ if __name__ == "__main__":
     
     parser.add_argument('--move', help="move to postition")
     parser.add_argument('--movr', help='shift by a set offset')
+    parser.add_argument('--speed', help="set speed")
+    parser.add_argument('--gmov', help='return movement settings (speed, acceleration, threshold, etc.)', action='store_true')
     parser.add_argument('--gser', help='return device serial number', action='store_true')
     parser.add_argument('--gets', help='return device state', action='store_true')
     parser.add_argument('--zero', help='sets the current position to 0', action='store_true')
+    parser.add_argument('--demo', help = 'move from 0 to 4500 with several speeds: 100, 500, 1000', action='store_true')
         
     args = parser.parse_args()
 
@@ -28,18 +31,56 @@ if __name__ == "__main__":
 
     if args.move is not None:
         motor_drive.move(int(args.move))
-        st = motor_drive.gets()
-        while st[5] & 0x80:
-            time.sleep(1)
-            st = motor_drive.gets()
+        motor_drive.wait_for_stop()
     elif args.movr is not None:
         motor_drive.movr(int(args.movr))
+    elif args.speed is not None:
+        motor_drive.smov(speed = args.speed)
+    elif args.gmov:
+        motor_drive.gmov()
     elif args.gser:
         print(motor_drive.gser())
     elif args.gets:
         motor_drive.gets()
     elif args.zero:
         motor_drive.zero()
+    elif args.demo:
+        run_demo(motor_drive)
     else:
         parser.print_help()
+
+def run_demo(motor : USB_8SMC5) -> None:
+    '''
+    Demo contains next steps:
+        1. Moving to 0
+        2. Setting speed to 100
+        3. Moving from 0 to 281
+        4. Setting speed to 500
+        5. Moving from 281 to 1405+281=1686
+        6. Settings speed to 1000
+        7. Moving from 1686 to 1686+2814=4500
+        8. Stop
+        9. Moving back with reverse repeat steps from 7 to 1
+        10. Stop (if need)
+    '''
+    motor.move(0)
+    motor.wait_for_stop()
+    motor.smov(speed = 100)
+    motor.movr(281)
+    motor.wait_for_dest(281)
+    motor.smov(speed = 500)
+    motor.movr(1405)
+    motor.wait_for_dest(281+1405)
+    motor.smov(speed = 1000)
+    motor.movr(2814)
+    motor.wait_for_dest(281+1405+2814)
+    # TODO go back with different speeds!
+    motor.smov(speed = 500)
+    motor.move(0)
+
+
+if __name__ == "__main__":
+    main()
+
+
     
